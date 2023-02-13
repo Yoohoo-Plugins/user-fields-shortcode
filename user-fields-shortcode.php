bc499cda95fede9573fc8baddfa2fa0a9aba2268
Andrew Lima <andrew@arctek.co.za>
2023-02-13 20:43:04 +0200

    Add some support for EDD.

--- user-fields-shortcode.php
+++ user-fields-shortcode.php
@@ -9,6 +9,8 @@
  * License: GPLv2 or later
  */
 
+
+
 function yh_user_fields_shortcode( $atts ) {
     global $current_user;
     // Return nothing if the user is logged-out.
@@ -19,11 +21,53 @@ function yh_user_fields_shortcode( $atts ) {
     // Get the key from the shortcode attribute.
     $key = esc_attr( $atts['key'] );
     
-    if ( empty( $value ) ) {
-        $value = $current_user->{$key};
+    $value = $current_user->{$key};
+
+    // If it's a date, let's format it better.
+    if ( $key === 'user_registered' ) {
+        $value = date_i18n( get_option( 'date_format' ), strtotime( $value ) );
     }
 
-    return $value;
+    return esc_html( $value );
 
 }
 add_shortcode( 'user_field', 'yh_user_fields_shortcode' );
+
+
+/**
+ * Shortcode for displaying EDD information
+ */
+function yh_edd_user_fields_shortcode( $atts ) {
+    global $current_user;
+    // Return nothing if the user is logged-out.
+    if ( ! is_user_logged_in() ) {
+        return;
+    }
+
+    // Make sure EDD is installed.
+    if ( ! class_exists( 'EDD_Customer' ) ) {
+        return;
+    }
+
+    ////// Possible Keys.
+    // $customer->name;
+    // $customer->email;
+    // $customer->date_created;
+    // $customer->purchase_count;
+    // $customer->purchase_value;
+    // $customer->id; // Customer ID
+    // $customer->user_id; // WordPress user ID
+    // $customer->payment_ids
+
+    // Get the key from the shortcode attribute.
+    $key = esc_attr( $atts['key'] );
+
+    // Customer information
+    $customer = new EDD_Customer( $current_user->ID );
+
+    $value = $customer->{$key};
+
+    return esc_html( $value );
+
+}
+add_shortcode( 'edd_user_field', 'yh_edd_user_fields_shortcode' );
